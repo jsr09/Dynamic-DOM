3ae740ce1b7a3854ec4aa84ea4ff17d66287ced5
jsr09 <97914865+jsr09@users.noreply.github.com>
2023-01-05 15:49:28 -0500

    Update 3

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+class Bookmark {
+    constructor (name, link){
+    this.name = name;
+    this.link = link;
 
+}  render(){
+
+}
+}
+// console.log(newBook)
+
+// creates link with input submit class
+const submit = document.body.querySelector('button');
+
+ //Function to pull user submitted user info
+ submit.addEventListener('click', getVal);
+
+function getVal(){
+    let name = document.getElementsByClassName('bookmarkName')[0].value;
+    let link  = document.getElementsByClassName('bookmarkLink')[0].value;
+    const newBook = new Bookmark(name, link);
+    newBook.render();
+console.log(newBook);
+    
+}
+
+
+ //function to filter between name and link
+            //push name and link into array
+//render function create list items
+
+   
